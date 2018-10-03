f61067d3c0646ac8cd7a098e0c602fd7c2741ab4
Torin Huff <torinhuff@yahoo.com>
2018-10-02 20:15:20 -0700

    this will return inputted text as is when the user hits the submit button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,66 @@
-var sentence = "What is happening";
+/*for (allWords) {
+  if (isWord)(word)) {
+    var newWord = translateWord(word);
+    print(newWord);
+  } else {
+    displayError();
+  }
+};
+
+function nonWord(word) {
+  var vowelAll = ["a", "e", "i", "o", "u", "y"];
+  var vowelMinus = ["a", "e", "i", "o", "u"];
+
+  if (word)
+};*/
+
+function convertPig(word) {
+  return word;
+}
+
+$(document).ready(function() {
+  $("form#pig-latin-converter").submit(function(event) {
+    event.preventDefault();
+    var inputString = $("input#pig-latin").val();
+    var outputString = convertPig(inputString);
+
+    $(".pig-sentence").text(outputString);
+    $("#result").show();
+
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+/*var sentence = "What is happening";
 var vowelAll = ["a", "e", "i", "o", "u", "y"];
 var vowelMinus = ["a", "e", "i", "o", "u"];
 
 var nonWord = function(word) {
   if (!sentence.includes(vowelAll)) {
-  return false;
+    return false;
+  } else {
+    return true;
   }
 };
+
+var includeVowel = function(anyvowel) {
+  if ()
+
+}
 
 var vowelStart = function(vow) {
   if (sentence.startsWith(vowelMinus)) {
     alert("starts with 'i'");
   }
 
-}
+}*/
